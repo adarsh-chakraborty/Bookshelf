cb61b6987499e1f42cc6a4eb9916592d1cbc70dc
Shrey Lawang <slawang108@gmail.com>
2021-12-07 22:33:00 +0530

    Added Files

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
-const DUMMY_DATA = [
-    {
-        "name": "Adarsn Chakraborty",
-        "title": "The Moonstone",
-        "author": "Wilkie Collins (1868)",
-        "price": "200",
-        "image": "https://upload.wikimedia.org/wikipedia/commons/2/26/The_Moonstone_1st_ed.jpg",
-    },
-    {
-        "name": "Shrey Lawang",
-        "title": "Harry Potter",
-        "author": "JK Rowlings",
-        "price": "250",
-        "image": "https://images-na.ssl-images-amazon.com/images/I/910ityjoncL.jpg",
-    },
-    {
-        "name": "Shrey Lawang",
-        "title": "War and Peace",
-        "author": "Leo Tolstoy",
-        "price": "500",
-        "image": "https://cdn.shopify.com/s/files/1/0100/4001/6992/products/War-_-Peace-Books-Online-Urdu-Bazaar-1613459309_360x.jpg?v=1625005278",
-    }      
-]   
 
+
+console.log('Sending a fetch request');
+
+
+
+
+console.log('Done fetching data');
 let TEMPLATE = "";
 // 2
 // i = 0
@@ -29,9 +13,11 @@ let TEMPLATE = "";
 // i = 2
 // For OF   
 
- for(let book of DUMMY_DATA)
+ function code(data)
  {
-     console.log("I is:",book, "Current title is:", book.title)    
+     for(let book of data)
+ {
+     console.log("I is:",book, "Current title is:", book.book)    
     TEMPLATE += `<div class="book-tabs">
                             <div class="book-image">
                                 <a href="#">
@@ -39,7 +25,7 @@ let TEMPLATE = "";
                                 </a>
                             </div>
                             <div class="book-details">
-                                <h5>${book.title}</h5>
+                                <h5>${book.book}</h5>
                                 <h6>${book.author}</h6>
                                 <h4>₹${book.price}</h4>
                             </div>
@@ -47,5 +33,19 @@ let TEMPLATE = "";
  }
 const booksGrid = document.querySelector('.grid-container');
 booksGrid.innerHTML = TEMPLATE;
+ }
+
+
+fetchBooks();
+
+async function fetchBooks(){
+    const res = await fetch('https://bookshelf.gq/api/books', {
+        method: 'PUT'
+    });
+    const data = await res.json();
+    code(data);
+
+}   
+
 
 
